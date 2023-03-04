4fe5f56040c59c3820238c5fffa8b94636ba1ac5
Aynoble <victordiya90@gmail.com>
2023-03-04 20:31:46 +0100

    commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://ayblog.herokuapp.com/api/",
+  baseURL: "https://blog-murex-five.vercel.app/api/",
 });
